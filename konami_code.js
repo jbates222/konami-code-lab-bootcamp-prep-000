b212f4c7843512ee3299fd622159eff357d1e904
Jackson Bates <jkbates222@gmail.com>
2018-09-27 18:49:32 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -20,21 +20,36 @@ var allowedKeys = {
   66: 'b'
 };
 
-var index = 0;
+var konamiCodePosition = 0;
 
-function init() {
- document.body.addEventListener('keydown', function(e) {
- var key = allowedKeys[e.keyCode];
- var requiredKey = codes[index];
- if (key === requiredKey) {
-   index++;
-   if (index == codes.length) {
-    alert("Congrats homie!!");
-    index = 0;
-  }
- } 
- else {
- index = 0;
+// add keydown event listener
+document.addEventListener('keydown', function(e) {
+  // get the value of the key code from the key map
+  var key = allowedKeys[e.keyCode];
+  // get the value of the required key from the konami code
+  var requiredKey = konamiCode[konamiCodePosition];
+
+  // compare the key with the required key
+  if (key == requiredKey) {
+
+    // move to the next key in the konami code sequence
+    konamiCodePosition++;
+
+    // if the last key is reached, activate cheats
+    if (konamiCodePosition == konamiCode.length) {
+      activateCheats();
+      konamiCodePosition = 0;
+    }
+  } else {
+    konamiCodePosition = 0;
   }
 });
+
+function activateCheats() {
+  document.body.style.backgroundImage = "url('images/cheatBackground.png')";
+
+  var audio = new Audio('audio/pling.mp3');
+  audio.play();
+
+  alert("cheats activated");
 }
