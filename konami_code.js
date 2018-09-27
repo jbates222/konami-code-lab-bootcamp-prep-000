f65a24c7542a28700d66e12b66adc81559e5ea3a
Jackson Bates <jkbates222@gmail.com>
2018-09-27 18:44:16 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,30 @@ const codes = [
   "a"
 ];
 
+var allowedKeys = {
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+  65: 'a',
+  66: 'b'
+};
+
+var index = 0;
+
 function init() {
-  // your code here
+ document.body.addEventListener('keydown', function(e) {
+ var key = allowedKeys[e.keyCode];
+ var requiredKey = codes[index];
+ if (key === requiredKey) {
+   index++;
+   if (index == codes.length) {
+    alert("Congrats homie!!");
+    index = 0;
+  }
+ } 
+ else {
+ index = 0;
+  }
+});
 }
